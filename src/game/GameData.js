bf8a0423ad39aca2d168b92c214259db84514d85
Holger Finger <hfinger@users.noreply.github.com>
2014-02-10 00:45:53 +0100

    error

--- src/game/GameData.js
+++ src/game/GameData.js
@@ -1,13 +1,7 @@
-var GameData = function(json) {
-    this.mapTypes = {};
-    this.objectTypes = {}; //use objTypeId as key for fast access
-    this.spritesheets = {};
-    this.maps =  {}; //use mapId as key for fast access;
-    this.users = {};
-
-    for(var key in json) {
-        if(json.hasOwnProperty(key)) {
-            this[key] = json[key];
-        }
-    }
+var GameData = function() {
+    this.mapTypes = new GameList(MapType.prototype);
+    this.objectTypes = new GameList(ObjectType.prototype);
+    this.spritesheets = new GameList(Spritesheet.prototype);
+    this.maps = new GameList(MapData.prototype);
+    this.users = new GameList(User.prototype);
 }
--- src/game/objectType.js
+++ src/game/objectType.js
@@ -1,12 +1,20 @@
-var ObjectType = function() {
+var ObjectType = function(objDesc) {
     this._id;
-    this.width;
-    this.height;
+    this.initWidth;
+    this.initHeight;
     this.allowOnMapTypeId = [];
     this.hasChildMapTypeId;
     this.name;
     this.spritesheetId;
     this.spriteFrame;
+
+    if (ObjectType.arguments.length == 1) {
+        for(var key in objDesc) {
+            if(objDesc.hasOwnProperty(key)) {
+                this[key] = objDesc[key];
+            }
+        }
+    }
 }
 
 ObjectType.prototype.getArea = function () {
