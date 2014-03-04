f7275b1b4e018ae9068627a9db0ab8044e9ad5ac
Holger Finger <hfinger@users.noreply.github.com>
2014-03-04 23:21:12 +0100

    changed remote path of game folder quadTree is correctly initialized coordinates are always relative to center

--- src/game/mapData.js
+++ src/game/mapData.js
@@ -1,11 +1,39 @@
 var MapData = function(objDesc) {
     this._id;
-    this.width;
-    this.height;
+    this.width = objDesc.width;
+    this.height = objDesc.height;
     this.mapId;
     this.mapTypeId;
-    var quadTree = new window.QuadTree({x:0, y:0, width:this.width, height:this.height},false);
+    var quadTree = new window.QuadTree({x:-this.width/2, y:-this.height/2, width:this.width, height:this.height},false); //private variable
     this.mapObjects = {};
+
+    ////////////////////////////////////////////////////////////////
+    // Privileged Member Functions: (they need to access quadTree)
+    ////////////////////////////////////////////////////////////////
+
+    this.addObjectToTree = function(mapObject) {
+        var treeItem = {
+            x: mapObject.x-mapObject.width/2,
+            y: mapObject.y-mapObject.height/2,
+            width: mapObject.width,
+            height: mapObject.height,
+            obj: mapObject
+        }
+        quadTree.insert(treeItem);
+    }
+
+    this.collisionDetection = function (mapObject) {
+
+        var items = quadTree.retrieve({x:mapObject.x-mapObject.width/2, y:mapObject.y-mapObject.height/2, height:mapObject.height, width:mapObject.width});
+
+        //TODO: detect orientations etc.
+
+        return items;
+    }
+
+    this.getQuadTree = function () {
+        return quadTree;
+    }
 
     if (MapData.arguments.length == 1) {
         for(var key in objDesc) {
@@ -17,8 +45,8 @@ var MapData = function(objDesc) {
                     else {
                         this.mapObjects = new GameList(MapObject,objDesc.mapObjects);
                     }
-                    for (obj in this.mapObjects) {
-                        addObjectToTree(obj);
+                    for (obj_id in this.mapObjects.hashList) {
+                        this.addObjectToTree(this.mapObjects.hashList[obj_id]);
                     }
                 }
                 else {
@@ -31,23 +59,7 @@ var MapData = function(objDesc) {
         this.mapObjects = new GameList(MapObject);
     }
 
-    function addObjectToTree(mapObject) {
-        var treeItem = {
-            x: mapObject.x,
-            y: mapObject.y,
-            width: mapObject.width,
-            height: mapObject.height,
-            obj: mapObject
-        }
-        quadTree.insert(treeItem);
-    }
-}
 
-MapData.prototype.collisionDetection = function (mapObject) {
 
-    var items = quad.retrieve({x:mapObject.x, y:mapObject.y, height:mapObject.height, width:mapObject.width});
 
-    //TODO: detect orientations etc.
-
-    return items;
 }
