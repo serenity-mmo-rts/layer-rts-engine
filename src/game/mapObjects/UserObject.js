bb86ef40efed08c38445267152cde216d4dd9410
Caspar Goeke <caspar-goeke@users.noreply.github.com>
2014-12-17 14:25:33 +0100

    Models Consumer, SubLayer, UserObject

---
+++ src/game/mapObjects/UserObject.js
@@ -0,0 +1,111 @@
+var node = !(typeof exports === 'undefined');
+if (node) {
+    var GameData = require('../GameData').GameData;
+    var MapObject = require('../MapObject').MapObject;
+}
+
+
+(function (exports) {
+
+    var userObjectStates = {};
+    userObjectStates.TEMP = 0;
+    userObjectStates.WORKING = 1;
+    userObjectStates.FINISHED = 2;
+
+
+    var UserObject = MapObject.extend({
+
+        init: function UserObject(gameData,initObj) {
+            // serialized:
+            this.userId = 0; // optional
+            this.state = userObjectStates.FINISHED;
+
+            // new member variables
+            this.level= 0;
+            this.range = 0;
+
+            this.freeItemSlots = 0;
+            this.freeUnitSlots = 0;
+            this.freeUpgradeSlots = 0;
+
+            this.totalHealthPoints = 0;
+            this.totalDefensePoints = 0;
+            this.totalOffensePoints = 0;
+
+            this.ownerIds = []; // String List of owner Ids
+
+            this.ressources = {}
+
+            // this.ressources['user5467']['carbon'] =  {
+            //    stored: 0,
+            //    inOutPerSecEff : 0,
+            //    inOutPerSecREq :0,
+            //    capacity :0
+            // }
+
+
+            this.units = []; // get example: carbonAmount = this.units['user5467']['supertank'];
+            this.items = [];
+            this.upgrade = [];
+
+            this.playerMoneyInOutPerSec = 0;
+            this.populationLoan =0;
+            this.populationPayments = 0;
+
+            this.activities = [];
+
+            // this.activities[activityID]=  {
+            //    attractivity : 0,
+            //    currentPopulation : 0,
+            //    peopleInOutPerSec : 0,
+            //    capacity : 0,
+            //    occupationRate : 0
+            //    }
+
+
+
+
+            // not serialized:
+            this.gameData = gameData;
+
+            // init:
+            if (UserObject.arguments.length == 2) {
+                this.load(initObj);
+            }
+        },
+
+        save: function () {
+            var o = this._super();
+            o.a2 =  [this.userId,
+                    this.state];
+            return o;
+        },
+
+        load: function (o) {
+            if (o.hasOwnProperty("a")) {
+                this._id = o._id;
+                this.mapId = o.mapId;
+                this.objTypeId = o.a[0];
+                this.x = o.a[1];
+                this.y = o.a[2];
+                this.width = o.a[3];
+                this.height = o.a[4];
+                this.userId = o.a[5];
+                this.state = o.a[6];
+            }
+            else {
+                for (var key in o) {
+                    if (o.hasOwnProperty(key)) {
+                        this[key] = o[key];
+                    }
+                }
+            }
+        }
+
+    });
+
+    exports.userObjectStates = userObjectStates;
+    exports.UserObject = UserObject;
+
+})(typeof exports === 'undefined' ? window : exports);
+
