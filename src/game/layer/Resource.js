65d9cd2cf6ff4e4b172602cbdf43ed3840ef4ec4
Holger Finger <hfinger@users.noreply.github.com>
2015-06-29 10:41:27 +0200

    added resource class structure

--- src/game/layer/Resource.js
+++ src/game/layer/Resource.js
@@ -1,3 +1,39 @@
-/**
- * Created by hofinger on 12/4/2014.
- */
+
+var node = !(typeof exports === 'undefined');
+if (node) {
+    var Class = require('../Class').Class;
+    var GameData = require('../GameData').GameData;
+    var GameList = require('../GameList').GameList;
+}
+
+
+
+(function (exports) {
+
+    var Resource = function (mapObj,initObj){
+
+        this._hubSystems = [];
+
+    };
+
+    Resource.prototype= {
+
+
+        save: function () {
+
+            var o = {};
+
+            return o;
+        },
+
+
+        load: function (o) {
+
+
+        }
+
+    }
+
+    exports.Resource = Resource;
+
+})(typeof exports === 'undefined' ? window : exports);
