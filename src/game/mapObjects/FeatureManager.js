6294010e4f5c3cdcb2956577b7b03c044117a451
Caspar Goeke <caspar-goeke@users.noreply.github.com>
2015-08-26 21:34:52 +0200

    feature Manager

--- src/game/mapObjects/FeatureManager.js
+++ src/game/mapObjects/FeatureManager.js
@@ -3,144 +3,118 @@ if (node) {
     var Class = require('../Class').Class;
     var GameData = require('../GameData').GameData;
     var MapObject = require('./../MapObject').MapObject;
-    var eventStates = require('../events/AbstractEvent').eventStates;
 }
 
 
 (function (exports) {
 
-
-
-    var FeatureManager = function (gameData,initObj){
+    var FeatureManager = function (mapObject,initObj){
 
         // serialized:
+        this._appliedFeatures = [];
+        this._change =false;
 
-        // only ids serialized
-        this.deployedItems = [];// get example: LaserTrooper = this.items['userId']['Index'];
-        this.appliedItems = {};
-
-        // not serialized
-        this.properties = {};
-        this.change =false;
-
-        this.updateObjectProperties();
+        // not seialized
+        this._mapObj = mapObject;
+        //this.updateObjectProperties();
     };
 
     FeatureManager.prototype= {
 
-        getItems: function (){
-            return this.deployedItems
-        },
-
-        addItem: function (item){
-            this.deployedItems.push(item);
-        },
-
-        addFeature: function(id,property,change,operator,mode){
-            this.change = true;
-            if (this.appliedItems.hasOwnProperty(id)){
-
-                var object = {
-                    property:property,
-                    change: change,
-                    operator: operator,
-                    mode: mode
-                };
-                this.appliedItems[id].push(object);
-
-            }
-            else{
-                this.appliedItems[id] = [];
-                var object = {
-                    property:property,
-                    change: change,
-                    operator: operator,
-                    mode: mode
-                };
-                this.appliedItems[id].push(object);
-
-            }
-
-        },
-
-        removeItemFromQueue: function(idx){
-            this.buildQueue.splice(idx,1);
-        },
-
-
-        updateObjectProperties: function () {
-            this.properties = {};
-            var initProp = {};
-            var prop = {};
-            var initProp = this.gameData.objectTypes.get(this.objTypeId)._buildingBlocks;
-
-            for (var attr in initProp) {
-                prop[attr] = initProp[attr];
-                this.properties[attr] = initProp[attr];
-            }
-            for (var id in this.appliedItems){
-                var item = this.appliedItems[id];
-                for (var i = 0;i<item.length;i++){
-                    var feat = item[i];
-                    if (feat.operator ==1) { // times
-                        if (feat.mode ==1) { // apply to baseline
-                            var change = prop[feat.property] * feat.change;
-                            this.properties[feat.property] += change;
-                        }
-                        else if (feat.mode==2){ // apply to stack
-                            var change = this.properties[feat.property] * feat.change;
-                            this.properties[feat.property] += change;
+        addFeature: function(itemId,itemOrObj,variables,blocks,operators,changes){
+            var blockValid = true;
+            var varValid = true;
+            // check if block and variable exit exist
+            for (var i = 0; i<blocks.length; i++){
+                if (itemOrObj==1) { // object
+                    if (!this._mapObj._blocks.hasOwnProperty(blocks[i])) {
+                        blockValid = false;
+                        if (!this._mapObj._blocks.hasOwnProperty(blocks[i]).hasOwnProperty(variables[i])) {
+                            var varValid = false;
                         }
                     }
-                    else if (feat.operator ==2) { // plus
-                        this.properties[feat.property] += feat.change;
+                }
+                else if(itemOrObj==2){
+                    var item = this.gameData.itemTypes.get(itemId)
+                    if (!item._blocks.hasOwnProperty(blocks[i])) {
+                        blockValid = false;
+                        if (!item._blocks.hasOwnProperty(blocks[i]).hasOwnProperty(variables[i])) {
+                            var varValid = false;
+                        }
                     }
                 }
             }
+
+            if (blockValid && varValid){
+                this.change = true;
+                var feature = {
+                    itemId: itemId,
+                    itemOrObj:itemOrObj,
+                    variables: variables,
+                    blocks: blocks,
+                    operators: operators,
+                    changes: changes
+                };
+                this._appliedFeatures.push(feature);
+            }
+
+
+
+        },
+
+        updateObjectProperties: function () {
+
+            for (var i=0; i< this._appliedFeatures.length; i++){
+                var feature = this._appliedFeatures[i];
+
+                for (var k=0; k< feature.blocks.length; k++) {
+                    var itemId = feature.itemId[k];
+                    var itemOrObj = feature.itemOrObj[k];
+                    var variable = feature.variables[k];
+                    var blockName = feature.blocks[k];
+                    var operator = feature.operators[k];
+                    var change = feature.changes[k];
+
+                    if (operator=="plus"){
+                        this._mapObj._blocks[blockName][variable]+= change;
+                        }
+                    else{
+                        var toAdd  = this._mapObj._blocks[blockName][variable]*change;
+                        this._mapObj._blocks[blockName][variable]= toAdd;
+                    }
+                }
+
+            }
+
         },
 
 
         save: function () {
 
 
-            var buildQueueIds = [];
-            for (var i=0; i<this.buildQueue.length ; i++) {
-                buildQueueIds.push(this.buildQueue[i]._id);
-            }
 
-            var appliedItemIds = [];
-            var appliedItemProps= [];
-            var appliedItemValues = [];
-            var appliedItemOperators = [];
-            var appliedItemModes = [];
+            var ItemIds = [];
+            var vars= [];
+            var blocks = [];
+            var operators = [];
+            var changes = [];
             var count = 0;
-            for (var id in this.appliedItems)  {
 
-                for (var k = 0; k<this.appliedItems[id].length;k++){
-                    var feature = this.appliedItems[id][k];
-                    appliedItemIds[count] = id;
-                    appliedItemProps[count]= feature.property;
-                    appliedItemValues[count] = feature.change;
-                    appliedItemOperators[count] = feature.operator;
-                    appliedItemModes[count] = feature.mode;
-                    count++
-                }
-            }
-
-            var deployedItemIds= [];
-            for (var i=0; i<this.deployedItems.length; i++) {
-                deployedItemIds.push(this.deployedItems[i]._id);
+            for (var k = 0; k<this._appliedFeatures.length;k++){
+                var feature = this._appliedFeatures[k];
+                vars[count]= feature.variables;
+                blocks[count] = feature.blocks;
+                operators[count] = feature.operators;
+                changes[count] = feature.changes;
+                count++
             }
 
             var o = {
-            a :  [this.userId,
-                this.healthPoints,
-                buildQueueIds,
-                appliedItemIds,
-                appliedItemProps,
-                appliedItemValues,
-                appliedItemOperators,
-                appliedItemModes
+            a :  [
+                vars,
+                blocks,
+                operators,
+                changes
             ]};
             return o;
         },
@@ -148,7 +122,6 @@ if (node) {
         load: function (o) {
             if (o.hasOwnProperty("a"))
             {
-                this._super(o);
                 if (o.hasOwnProperty("a2"))
                 {
                     this.userId = o.a2[0];
